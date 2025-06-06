0d033ff8104b070f3fb43ee94d544b2389abbc3a
Dhanush Doddapaneni <dhanushdoddapaneni21@gmail.com>
2025-06-06 12:00:59 +0530

    defined the pydantic models for user registration and token authentication

---
+++ app/schemas.py
@@ -0,0 +1,10 @@
+from pydantic import BaseModel,EmailStr
+from typing import Optional
+
+class UserRegister(BaseModel):
+    email : EmailStr
+    password : str
+    
+class Tokem(BaseModel):
+    access_token:str
+    token_type:str
